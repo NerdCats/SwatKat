e2bf9987a23cc00daebb5a479c4697742a959456
Fahad Wajed <fahadwajed@gmail.com>
2016-03-30 12:56:09 +0600

    minor fixes

--- app/src/main/java/co/gobd/tracker/ui/LoginActivity.java
+++ app/src/main/java/co/gobd/tracker/ui/LoginActivity.java
@@ -1,9 +1,8 @@
 package co.gobd.tracker.ui;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
