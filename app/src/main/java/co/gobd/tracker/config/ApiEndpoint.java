d2582397983f826f8c19a6f2491f980ba31385e4
Fahad Wajed <fahadwajed@gmail.com>
2016-04-07 13:47:25 +0600

    ApiEndpoint changed

--- app/src/main/java/co/gobd/tracker/config/ApiEndpoint.java
+++ app/src/main/java/co/gobd/tracker/config/ApiEndpoint.java
@@ -5,7 +5,7 @@ package co.gobd.tracker.config;
  */
 public class ApiEndpoint {
 
-    public static final String PATH_PING_BASE_URL = "http://gobdshadowcat.cloudapp.net:80";
+    public static final String PATH_PING_BASE_URL = "http://gofetch.cloudapp.net:1337";
     public static final String PATH_TRACKER_LOCATION = "/api/ping";
     public static final String PATH_LOGIN_BASE_URL = "http://gofetch.cloudapp.net:80";
     public static final String PATH_LOGIN = "/token";
