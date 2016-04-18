bd9567e02f06bc4a14c7d90c7312bc57f29c9a9f
Fahim Masud Choudhury <fahim@gobd.co>
2016-04-18 15:39:57 +0600

    Fix: Removes context from SessionManagrImpl. It is not needed here as Dagger is creating the SharedPref instance

--- app/src/main/java/co/gobd/tracker/di/module/AppModule.java
+++ app/src/main/java/co/gobd/tracker/di/module/AppModule.java
@@ -3,7 +3,6 @@ package co.gobd.tracker.di.module;
 import android.app.Application;
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.preference.PreferenceManager;
 
 import javax.inject.Singleton;
 
@@ -57,8 +56,8 @@ public class AppModule {
 
     @Provides
     @Singleton
-    public SessionManager providesSessionManager(Context context, SharedPreferences sharedPreferences){
-        return new SessionManagerImpl(context, sharedPreferences);
+    public SessionManager providesSessionManager(SharedPreferences sharedPreferences){
+        return new SessionManagerImpl(sharedPreferences);
     }
 
 
