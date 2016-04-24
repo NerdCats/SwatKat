b54fcd16061c5a04fec0bd62f5cc6c27539c751a
fahad <fahadwajed@gmail.com>
2016-04-24 17:57:50 +0600

    GFETCH-118 added signup activity

---
+++ app/src/main/java/co/gobd/tracker/ui/activity/SignUpActivity.java
@@ -0,0 +1,15 @@
+package co.gobd.tracker.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import co.gobd.tracker.R;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
