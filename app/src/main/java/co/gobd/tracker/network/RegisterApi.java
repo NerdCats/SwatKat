9bd5e5f2e4612a40ee367b53900e0a3bc1e5091f
Fahad <fahadwajed@gmail.com>
2016-04-26 16:54:11 +0600

    GFETCH-124 cleanup

--- app/src/main/java/co/gobd/tracker/network/RegisterApi.java
+++ app/src/main/java/co/gobd/tracker/network/RegisterApi.java
@@ -1,7 +1,16 @@
 package co.gobd.tracker.network;
 
+import co.gobd.tracker.config.BackendUrl;
+import co.gobd.tracker.model.register.Register;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.Field;
+import retrofit2.http.POST;
+
 /**
  * Created by fahad on 4/26/16.
  */
 public interface RegisterApi {
+    @POST(BackendUrl.TaskCat.GET_REGISTER)
+    Call<Register> registerAsset(@Body Register register);
 }
