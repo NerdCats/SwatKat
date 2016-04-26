7fc4e7681ccfd512adf240bdd0e87a401326a212
Fahim Masud Choudhury <fahim@gobd.co>
2016-04-26 13:43:25 +0600

    Minor fix, project builds successfully

--- app/src/main/java/co/gobd/tracker/ui/activity/MainActivity.java
+++ app/src/main/java/co/gobd/tracker/ui/activity/MainActivity.java
@@ -5,7 +5,6 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.location.LocationManager;
-import android.media.Image;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -14,7 +13,6 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
