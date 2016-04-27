541da2cf38fc88430124eb103fcee92e5059593d
Fahad <fahadwajed@gmail.com>
2016-04-27 18:56:44 +0600

    MASTER UPDATE_INTERVAL_IN_MILLISECONDS changed

--- app/src/main/java/co/gobd/tracker/ui/service/LocationService.java
+++ app/src/main/java/co/gobd/tracker/ui/service/LocationService.java
@@ -30,7 +30,7 @@ import co.gobd.tracker.utility.SessionManager;
 
 public class LocationService extends Service implements GoogleApiClient.ConnectionCallbacks,
         GoogleApiClient.OnConnectionFailedListener, LocationListener {
-    public static final long UPDATE_INTERVAL_IN_MILLISECONDS = 60000;
+    public static final long UPDATE_INTERVAL_IN_MILLISECONDS = 30000;
 
     public static final long FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS =
             UPDATE_INTERVAL_IN_MILLISECONDS / 2;
