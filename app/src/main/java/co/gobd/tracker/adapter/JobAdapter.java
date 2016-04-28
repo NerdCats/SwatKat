1a28c5ab75f737efec3ea7b5d151b277e876fea0
Fahad <fahadwajed@gmail.com>
2016-04-28 17:47:13 +0600

    GFETCH-109 adds minor fix

--- app/src/main/java/co/gobd/tracker/adapter/JobAdapter.java
+++ app/src/main/java/co/gobd/tracker/adapter/JobAdapter.java
@@ -1,24 +1,62 @@
 package co.gobd.tracker.adapter;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import javax.inject.Inject;
+
 import co.gobd.tracker.R;
+import co.gobd.tracker.model.job.AssignedJob;
 import co.gobd.tracker.model.job.JobModel;
+import co.gobd.tracker.service.job.JobCallback;
+import co.gobd.tracker.service.job.JobService;
+import co.gobd.tracker.ui.view.OnItemClickListener;
+import co.gobd.tracker.utility.SessionManager;
 
 /**
  * Created by fahad on 4/28/16.
  */
 public class JobAdapter extends RecyclerView.Adapter<JobAdapter.JobViewHolder> {
 
+
+    private JobService jobService;
     private List<JobModel> jobModelList;
-    private AdapterView.OnItemClickListener onItemClickListener;
+    private OnItemClickListener onItemClickListener;
+    private Context context;
+
+    public JobAdapter(final Context context, JobService jobService, String bearer, String assetId){
+        this.jobService = jobService;
+        this.context = context;
+        this.jobModelList = new ArrayList<>();
+
+
+        jobService.getAssignedJobList(bearer, assetId, new JobCallback() {
+            @Override
+            public void onGetJobSuccess(AssignedJob assignedJob) {
+                if(assignedJob != null){
+                    jobModelList = assignedJob.getJobModelList();
+                    notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onGetJobFailure() {
+
+            }
+
+            @Override
+            public void onConnectionError() {
+
+            }
+        });
+    }
 
     public class JobViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
         TextView name, state;
@@ -37,10 +75,14 @@ public class JobAdapter extends RecyclerView.Adapter<JobAdapter.JobViewHolder> {
             int position = getLayoutPosition();
             if(jobModelList.isEmpty()){
                 if(onItemClickListener != null){
-                    onItemClickListener.on
+                    onItemClickListener.onClick(v, position);
                 }
             }
         }
+    }
+
+    public void setOnItemClickListener(final OnItemClickListener onItemClickListener) {
+        this.onItemClickListener = onItemClickListener;
     }
 
     public JobAdapter(List<JobModel> jobModelList){
