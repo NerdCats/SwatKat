f19092dacb029c65a9542634641c4dd67aa8958b
Fahad <fahadwajed@gmail.com>
2016-06-01 12:57:07 +0600

    fixes minor ui issues

--- app/src/main/java/co/gobd/tracker/ui/activity/MainActivity.java
+++ app/src/main/java/co/gobd/tracker/ui/activity/MainActivity.java
@@ -222,12 +222,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
-    public void onGetJobButtonClick(View view) {
-        Intent intent = new Intent(this, JobActivity.class);
-        startActivity(intent);
-        finish();
-
-    }
 
     public void onSignOutButtonClick(View view) {
         stopLocationService();
