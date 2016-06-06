36e8fafea1c2f8de6c097e20441e9fd2afab82de
Fahad <fahadwajed@gmail.com>
2016-06-06 19:53:44 +0600

    implements Parcelable in AssignedJob

--- app/src/main/java/co/gobd/tracker/model/job/AssignedJob.java
+++ app/src/main/java/co/gobd/tracker/model/job/AssignedJob.java
@@ -1,11 +1,14 @@
 package co.gobd.tracker.model.job;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.List;
 
 /**
  * Created by fahad on 4/25/16.
  */
-public class AssignedJob {
+public class AssignedJob implements Parcelable {
     List<JobModel> jobModelList;
 
     public AssignedJob(List<JobModel> jobModelList) {
@@ -27,5 +30,32 @@ public class AssignedJob {
 
     public void setJobModelList(List<JobModel> jobModelList) {
         this.jobModelList = jobModelList;
+    }
+
+    public static final Parcelable.Creator<AssignedJob> CREATOR
+            = new Parcelable.Creator<AssignedJob>(){
+        @Override
+        public AssignedJob createFromParcel(Parcel source) {
+            return new AssignedJob(source);
+        }
+
+        @Override
+        public AssignedJob[] newArray(int size) {
+            return new AssignedJob[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeTypedList(jobModelList);
+    }
+
+    private AssignedJob(Parcel in){
+        in.readTypedList(jobModelList, JobModel.CREATOR);
     }
 }
