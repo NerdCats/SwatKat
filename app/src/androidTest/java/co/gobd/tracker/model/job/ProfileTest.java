33c13de94d6c59b30339779165a6bc9e560af436
Fahad <fahadwajed@gmail.com>
2016-06-07 15:04:57 +0600

    adds test class for Profile

---
+++ app/src/androidTest/java/co/gobd/tracker/model/job/ProfileTest.java
@@ -0,0 +1,19 @@
+package co.gobd.tracker.model.job;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by fahadwajed on 6/7/16.
+ */
+public class ProfileTest {
+    @org.junit.Before
+    public void setUp() throws Exception {
+
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+
+    }
+
+}
