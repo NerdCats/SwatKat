a989444c2b9a11eecb57cdc25321f50ee3cffb40
Fahim Masud Choudhury <fahim@gobd.co>
2016-06-29 12:29:46 +0600

    GFETCH-180 Header image added in navigation drawer

--- app/src/main/java/co/gobd/tracker/ui/activity/LoginActivity.java
+++ app/src/main/java/co/gobd/tracker/ui/activity/LoginActivity.java
@@ -3,7 +3,6 @@ package co.gobd.tracker.ui.activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
--- app/src/main/java/co/gobd/tracker/ui/activity/MainActivity.java
+++ app/src/main/java/co/gobd/tracker/ui/activity/MainActivity.java
@@ -39,201 +39,182 @@ import javax.inject.Inject;
 
 public class MainActivity extends AppCompatActivity implements OnJobItemClickListener {
 
-    private static final String LOG_TAG = MainActivity.class.getSimpleName();
-    @Inject
-    SessionManager sessionManager;
-    @Inject
-    JobService jobService;
-    @Inject
-    Context context;
-    private ImageButton ibToggleStartStop;
-    private Button btnMap;
+  private static final String LOG_TAG = MainActivity.class.getSimpleName();
+  @Inject SessionManager sessionManager;
+  @Inject JobService jobService;
+  @Inject Context context;
+  private ImageButton ibToggleStartStop;
+  private Button btnMap;
 
-    private List<JobModel> jobModelList = new ArrayList<>();
-    private RecyclerView recyclerView;
-    private JobAdapter jobAdapter;
-    private Toolbar toolbar;
+  private List<JobModel> jobModelList = new ArrayList<>();
+  private RecyclerView recyclerView;
+  private JobAdapter jobAdapter;
+  private Toolbar toolbar;
 
-    private NavigationView nvDrawer;
-    private ActionBarDrawerToggle drawerToggle;
-    private DrawerLayout drawerLayout;
+  private NavigationView nvDrawer;
+  private ActionBarDrawerToggle drawerToggle;
+  private DrawerLayout drawerLayout;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+  @Override protected void onCreate(Bundle savedInstanceState) {
 
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        ((GoAssetApplication) getApplication()).getComponent().inject(this);
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    ((GoAssetApplication) getApplication()).getComponent().inject(this);
 
-        // Toolbar setup
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        /*if (getSupportActionBar() != null) {
-            getSupportActionBar().setDisplayHomeAsUpEnabled(false);
-        }*/
-
-        // Sets the drawer view
-        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-
-        String assetId = sessionManager.getAssetId();
-        String bearer = sessionManager.getBearer();
-
-        recyclerView = (RecyclerView) findViewById(R.id.rv_joblist);
-
-        jobAdapter = new JobAdapter(context, jobService, bearer, assetId);
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
-        recyclerView.setLayoutManager(layoutManager);
-        recyclerView.setItemAnimator(new DefaultItemAnimator());
-        recyclerView.setAdapter(jobAdapter);
-
-        jobAdapter.setOnJobItemClickListener(this);
-        TextView tvAssetName = (TextView) findViewById(R.id.tvAssetName);
-        String assetName = sessionManager.getUsername();
-        tvAssetName.setText(assetName);
-
-        ibToggleStartStop = (ImageButton) findViewById(R.id.ib_toggle_location);
-
-
-        ibToggleStartStop.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (isLocationServiceRunning(LocationService.class)) {
-                    stopLocationService();
-                    ibToggleStartStop.setImageResource(R.drawable.ic_play_circle_filled_green_24dp);
-                } else {
-                    startLocationService();
-                    ibToggleStartStop.setImageResource(R.drawable.ic_pause_circle_filled_red_24dp);
-                }
-
-            }
-        });
+    // Toolbar setup
+    toolbar = (Toolbar) findViewById(R.id.toolbar);
+    setSupportActionBar(toolbar);
+    if (getSupportActionBar() != null) {
+      getSupportActionBar().setDisplayShowHomeEnabled(true);
+      getSupportActionBar().setDisplayHomeAsUpEnabled(false);
     }
 
+    // Sets the drawer view
+    drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        checkLocationStatus();
+    String assetId = sessionManager.getAssetId();
+    String bearer = sessionManager.getBearer();
+
+    recyclerView = (RecyclerView) findViewById(R.id.rv_joblist);
+
+    jobAdapter = new JobAdapter(context, jobService, bearer, assetId);
+    RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+    recyclerView.setLayoutManager(layoutManager);
+    recyclerView.setItemAnimator(new DefaultItemAnimator());
+    recyclerView.setAdapter(jobAdapter);
+
+    jobAdapter.setOnJobItemClickListener(this);
+    TextView tvAssetName = (TextView) findViewById(R.id.tvAssetName);
+    String assetName = sessionManager.getUsername();
+    tvAssetName.setText(assetName);
+
+    ibToggleStartStop = (ImageButton) findViewById(R.id.ib_toggle_location);
+
+    ibToggleStartStop.setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View v) {
         if (isLocationServiceRunning(LocationService.class)) {
-            ibToggleStartStop.setImageResource(R.drawable.ic_pause_circle_filled_red_24dp);
+          stopLocationService();
+          ibToggleStartStop.setImageResource(R.drawable.ic_play_circle_filled_green_24dp);
         } else {
-            ibToggleStartStop.setImageResource(R.drawable.ic_play_circle_filled_green_24dp);
+          startLocationService();
+          ibToggleStartStop.setImageResource(R.drawable.ic_pause_circle_filled_red_24dp);
         }
-    }
+      }
+    });
+  }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+  @Override protected void onResume() {
+    super.onResume();
+    checkLocationStatus();
+    if (isLocationServiceRunning(LocationService.class)) {
+      ibToggleStartStop.setImageResource(R.drawable.ic_pause_circle_filled_red_24dp);
+    } else {
+      ibToggleStartStop.setImageResource(R.drawable.ic_play_circle_filled_green_24dp);
+    }
+  }
+
+  @Override public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.menu_main, menu);
+    return true;
+  }
+
+  @Override protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+    super.onPostCreate(savedInstanceState);
+  }
+
+  @Override public boolean onOptionsItemSelected(MenuItem item) {
+    // Handle action bar item clicks here. The action bar will
+    // automatically handle clicks on the Home/Up button, so long
+    // as you specify a parent activity in AndroidManifest.xml.
+    switch (item.getItemId()) {
+      case android.R.id.home:
+        drawerLayout.openDrawer(GravityCompat.START);
         return true;
     }
 
-    @Override protected void onPostCreate(@Nullable Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
+    return super.onOptionsItemSelected(item);
+  }
+
+  /**
+   * Checks if GPS is enabled or not.
+   */
+  public void checkLocationStatus() {
+    Log.i(LOG_TAG, "Checking GPS Status");
+    LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+    if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+      Toast.makeText(this, "Location is enabled", Toast.LENGTH_SHORT).show();
+    } else {
+      showGPSDisabledAlertToUser();
     }
+  }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        switch (item.getItemId())
-        {
-            case android.R.id.home:
-                drawerLayout.openDrawer(GravityCompat.START);
-                return true;
-        }
+  /**
+   * Alert Dialog to enable GPS
+   */
 
-        return super.onOptionsItemSelected(item);
+  public void showGPSDisabledAlertToUser() {
+    AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+    alertDialogBuilder.setMessage(
+        "Location is disabled in your device. Would you like to enable it?")
+        .setCancelable(false)
+        .setPositiveButton("Enable", new DialogInterface.OnClickListener() {
+          public void onClick(DialogInterface dialog, int id) {
+            Intent callGPSSettingIntent =
+                new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            startActivity(callGPSSettingIntent);
+          }
+        });
+    alertDialogBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+      public void onClick(DialogInterface dialog, int id) {
+        //dialog.cancel();
+        MainActivity.this.finish();
+      }
+    });
+    AlertDialog alert = alertDialogBuilder.create();
+    alert.show();
+  }
+
+  /**
+   * Checks if the service is running or not
+   */
+
+  private boolean isLocationServiceRunning(Class<?> serviceClass) {
+    ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+    for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(
+        Integer.MAX_VALUE)) {
+      if (serviceClass.getName().equals(service.service.getClassName())) {
+        return true;
+      }
     }
+    return false;
+  }
 
-    /**
-     * Checks if GPS is enabled or not.
-     */
-    public void checkLocationStatus() {
-        Log.i(LOG_TAG, "Checking GPS Status");
-        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-        if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-            Toast.makeText(this, "Location is enabled", Toast.LENGTH_SHORT).show();
-        } else {
-            showGPSDisabledAlertToUser();
-        }
-    }
+  /**
+   * Starts the location service that enables Google location update
+   */
+  public void startLocationService() {
+    Intent intent = new Intent(this, LocationService.class);
+    startService(intent);
+  }
 
-    /**
-     * Alert Dialog to enable GPS
-     */
+  public void stopLocationService() {
+    Intent intent = new Intent(this, LocationService.class);
+    stopService(intent);
+  }
 
-    public void showGPSDisabledAlertToUser() {
-        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
-        alertDialogBuilder.setMessage("Location is disabled in your device. Would you like to enable it?")
-                .setCancelable(false)
-                .setPositiveButton("Enable",
-                        new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int id) {
-                                Intent callGPSSettingIntent = new Intent(
-                                        android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                                startActivity(callGPSSettingIntent);
-                            }
-                        });
-        alertDialogBuilder.setNegativeButton("Cancel",
-                new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        //dialog.cancel();
-                        MainActivity.this.finish();
-                    }
-                });
-        AlertDialog alert = alertDialogBuilder.create();
-        alert.show();
-    }
+  public void onSignOutButtonClick(View view) {
+    stopLocationService();
+    sessionManager.clearAll();
+    Intent intent = new Intent(this, LoginActivity.class);
+    startActivity(intent);
+    finish();
+  }
 
-    /**
-     * Checks if the service is running or not
-     *
-     * @param serviceClass
-     * @return
-     */
+  @Override public void onItemClick(View view, int position, JobModel jobModel) {
 
-    private boolean isLocationServiceRunning(Class<?> serviceClass) {
-        ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
-        for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
-            if (serviceClass.getName().equals(service.service.getClassName())) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-
-    /**
-     * Starts the location service that enables Google location update
-     */
-    public void startLocationService() {
-        Intent intent = new Intent(this, LocationService.class);
-        startService(intent);
-    }
-
-    public void stopLocationService() {
-        Intent intent = new Intent(this, LocationService.class);
-        stopService(intent);
-    }
-
-    public void onSignOutButtonClick(View view) {
-        stopLocationService();
-        sessionManager.clearAll();
-        Intent intent = new Intent(this, LoginActivity.class);
-        startActivity(intent);
-        finish();
-    }
-
-
-    @Override
-    public void onItemClick(View view, int position, JobModel jobModel) {
-
-        Intent intent = new Intent(this, JobActivity.class);
-        intent.putExtra("JobModel", jobModel);
-        startActivity(intent);
-
-
-    }
+    Intent intent = new Intent(this, JobActivity.class);
+    intent.putExtra("JobModel", jobModel);
+    startActivity(intent);
+  }
 }
