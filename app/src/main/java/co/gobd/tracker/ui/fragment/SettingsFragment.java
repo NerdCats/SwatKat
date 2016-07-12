a5df022d34d681114357bf1274cf21d41cff0875
Fahad Wajed <fahadwajed@gmail.com>
2016-07-12 17:30:10 +0600

    GFETCH-189 adds preference fragments

---
+++ app/src/main/java/co/gobd/tracker/ui/fragment/SettingsFragment.java
@@ -0,0 +1,35 @@
+package co.gobd.tracker.ui.fragment;
+
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import co.gobd.tracker.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SettingsFragment extends PreferenceFragment {
+
+
+    public SettingsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_settings, container, false);
+    }
+
+}
