6a946106ea68c774b1de3a0bec70c9f038ad23ee
Fahad Wajed <fahadwajed@gmail.com>
2016-10-25 17:53:54 +0600

    adds SecureDelivery model

---
+++ app/src/main/java/co/gobd/tracker/model/job/task/SecureDeliveryTask.java
@@ -0,0 +1,46 @@
+package co.gobd.tracker.model.job.task;
+
+import co.gobd.tracker.model.job.Location;
+
+/**
+ * Created by fahad on 10/25/16.
+ */
+
+public class SecureDeliveryTask extends JobTask {
+
+    private String Id;
+    private String JobTaskStateString;
+    private String State;
+
+    public SecureDeliveryTask(String jobTaskStateString, String state, String Id) {
+        super(JobTaskTypes.SECURE_DELIVERY, "Cash Delivery");
+        JobTaskStateString = jobTaskStateString;
+        State = state;
+        this.Id = Id;
+        setType(JobTaskTypes.SECURE_DELIVERY);
+    }
+
+    public String getJobTaskStateString() {
+        return JobTaskStateString;
+    }
+
+    public String getState() {
+        return State;
+    }
+
+    public void setState(String State) {
+        this.State = State;
+    }
+
+    public void setId(String Id){
+        this.Id = Id;
+    }
+
+    public String getId(){
+        return Id;
+    }
+
+    public Location getLocation(){
+        return null;
+    }
+}
