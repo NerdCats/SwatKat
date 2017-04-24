56a2fc22ece6d7e2f9a935ea505600fb3699d6e0
isratmity <mity.crystal@gmail.com>
2017-04-24 16:37:28 +0600

    working on expandable list

--- app/src/main/java/co/gobd/tracker/ui/activity/TasksActivity.java
+++ app/src/main/java/co/gobd/tracker/ui/activity/TasksActivity.java
@@ -1,13 +1,17 @@
 package co.gobd.tracker.ui.activity;
 
 import android.content.Context;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.ExpandableListView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -17,7 +21,7 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
 import co.gobd.tracker.R;
-import co.gobd.tracker.adapter.JobAdapter;
+import co.gobd.tracker.adapter.Job_expandable_list_adapter;
 import co.gobd.tracker.adapter.OnCallClickListener;
 import co.gobd.tracker.adapter.OnTaskUpdateClickListener;
 import co.gobd.tracker.application.GoAssetApplication;
@@ -25,7 +29,6 @@ import co.gobd.tracker.model.job.JobModel;
 import co.gobd.tracker.presenter.TasksPresenter;
 import co.gobd.tracker.ui.view.OnJobItemClickListener;
 import co.gobd.tracker.ui.view.TasksView;
-import co.gobd.tracker.ui.view.TasksoverView;
 
 public class TasksActivity extends AppCompatActivity implements TasksView,OnJobItemClickListener,OnCallClickListener,OnTaskUpdateClickListener{
     @Inject
@@ -33,12 +36,12 @@ public class TasksActivity extends AppCompatActivity implements TasksView,OnJobI
     @Inject
     Context context;
 
-    @BindView(R.id.rv_joblist)
-    RecyclerView jobView;
+    @BindView(R.id.simple_expandable_listview)
+    ExpandableListView jobView;
     @BindView(R.id.toolbar)
     Toolbar toolbar;
 
-    JobAdapter jobAdapter;
+    Job_expandable_list_adapter job_expandable_list_adapter;
     private Unbinder unbinder;
     String TaskType;
 
@@ -50,26 +53,37 @@ public class TasksActivity extends AppCompatActivity implements TasksView,OnJobI
         ((GoAssetApplication) getApplication()).getComponent().inject(this);
         //  overViewPresenter.initialise(this);
         TaskType=getIntent().getStringExtra("TaskType");
-        jobAdapter = new JobAdapter(context);
+
         setupToolbar();
+        job_expandable_list_adapter = new Job_expandable_list_adapter(context);
         tasksPresenter.initialise(this);
         tasksPresenter.setInheritedTasktype(TaskType);
         tasksPresenter.loadAdapterData();
 
-        jobAdapter.setOnJobItemClickListener(this);
-        jobAdapter.setOnTaskUpdateClickListener(this);
-        jobAdapter.setOnCallClickListener(this);
+    //    jobAdapter.setOnJobItemClickListener(this);
+    //    jobAdapter.setOnTaskUpdateClickListener(this);
+     //   jobAdapter.setOnCallClickListener(this);
 
-        setupRecyclerView(jobAdapter);
+
+
+        jobView.setOnGroupExpandListener(new ExpandableListView.OnGroupExpandListener() {
+            int previousItem = -1;
+
+            @Override
+            public void onGroupExpand(int groupPosition) {
+                if(groupPosition != previousItem )
+                    jobView.collapseGroup(previousItem );
+                previousItem = groupPosition;
+            }
+        });
 
       //  setupNavigationHeaderView(sessionManager.getUsername());
 
         //fabToggleTracking.setOnClickListener(this);
     }
 
-    private void setupRecyclerView(JobAdapter jobAdapter) {
-        jobView.setLayoutManager(new LinearLayoutManager(context));
-        jobView.setItemAnimator(new DefaultItemAnimator());
+    private void setupRecyclerView(Job_expandable_list_adapter jobAdapter) {
+
         jobView.setAdapter(jobAdapter);
     }
     private void setupToolbar() {
@@ -81,7 +95,11 @@ if(getSupportActionBar()!=null) {
     }
     @Override
     public void setJobModelList(List<JobModel> jobModelList) {
-        jobAdapter.setAdapterData(jobModelList);
+        job_expandable_list_adapter.setAdapterData(jobModelList);
+        if(jobModelList.size()!=0)
+        {  setupRecyclerView(job_expandable_list_adapter);
+
+        }
     }
 
     @Override
