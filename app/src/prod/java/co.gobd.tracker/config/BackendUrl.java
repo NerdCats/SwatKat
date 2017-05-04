f9784c3a2a8699f84f684858ea8064350b4b604a
isratmity <mity.crystal@gmail.com>
2017-05-04 11:59:16 +0600

    changed api in prod

--- app/src/prod/java/co.gobd.tracker/config/BackendUrl.java
+++ app/src/prod/java/co.gobd.tracker/config/BackendUrl.java
@@ -15,14 +15,17 @@ public class BackendUrl {
 
     // Main backend of go fetch
     public static final class TaskCat {
-        //Production
-        public static final String BASE = "http://gofetch.cloudapp.net";
-        //public static final String BASE = "http://fetchprod.gobd.co";
-        // Path
-        public static final String LOGIN = "/token";
-        public static final String GET_PROFILE = "/api/Account/Profile";
+        // Dev
+        public static final String BASE = "http://fetchprod.gobd.co";
+        //public static final String BASE = "http://fetchdev.gobd.co";
+        // Path5
+        public static final String LOGIN = "/api/auth/token";
         public static final String GET_REGISTER = "/api/Account/Register";
-        public static final String GET_ASSIGNED_JOBS = "/api/Account/{userId}/jobs";
+        //  public static final String GET_ASSIGNED_JOBS = "/api/job/jobsbyasset/{userId}?envelope=true&page=0&pageSize=10";
+        public static final String GET_PROFILE = "/api/Account/Profile";
+        public static final String GET_ASSIGNED_JOBS = "/api/job/odata";
+
+       // public static final String GET_ASSIGNED_JOBS = "/api/job/odata?$filter=(Tasks/any(task: task/State eq 'IN_PROGRESS' and Task/Type eq 'Delivery' and task/AssetRef eq '{userId}'))"+"?&pageSize=50&page=0&sortDirection=Desc";
         public static final String PATCH_TASK_STATE = "/api/Job/{jobId}/{taskId}";
 
     }
